6d65403ba179fc6407bcadddd63bcc2640dc0f0e
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2019-05-13 11:21:55 -0400

    We can now send malformed ARP requests.

--- src/drivers/vnet.c
+++ src/drivers/vnet.c
@@ -4,6 +4,7 @@
 /* This driver relies on a host OS anyway. */
 #include <stdio.h>
 
+#include "../ether.h"
 #include "../mem.h"
 #ifdef NET_CON_ECHO
 #include "../bstrlib.h"
@@ -13,13 +14,13 @@
 #include <sys/socket.h>
 #include <net/if.h>
 #include <sys/ioctl.h>
-#include <netinet/ether.h>
+//#include <netinet/ether.h>
 #include <unistd.h>
 
 /* Open a socket and get the MAC address the socket will send from. */
-int net_open_socket( bstring ifname, uint8_t mac_addr[6] ) {
+int net_open_socket( bstring ifname, int* if_idx, uint8_t mac_addr[6] ) {
    int sockfd_out = 0;
-   struct ifreq if_idx;
+   struct ifreq if_idx_req;
    struct ifreq if_mac;
    char* if_name_c = NULL;
    size_t if_name_len = 0;
@@ -35,14 +36,15 @@ int net_open_socket( bstring ifname, uint8_t mac_addr[6] ) {
    if_name_len = blength( ifname );
 
    /* Get the sending interface index. */
-   memset( &if_idx, 0, sizeof( struct ifreq ) );
-   strncpy( if_idx.ifr_name, if_name_c, if_name_len );
-   if( 0 > ioctl( sockfd_out, SIOCGIFINDEX, &if_idx ) ) {
+   memset( &if_idx_req, 0, sizeof( struct ifreq ) );
+   strncpy( if_idx_req.ifr_name, if_name_c, if_name_len );
+   if( 0 > ioctl( sockfd_out, SIOCGIFINDEX, &if_idx_req ) ) {
       do_error( "SIOCGIFINDEX\n" );
       close( sockfd_out );
       sockfd_out = -1;
       goto cleanup;
    }
+   *if_idx = if_idx_req.ifr_ifindex;
 
    /* Get the sending interface MAC to return. */
    memset( &if_mac, 0, sizeof( struct ifreq ) );
@@ -58,12 +60,37 @@ cleanup:
    return sockfd_out;
 }
 
-int net_send( bstring buffer ) {
+int net_send_packet( 
+   int socket, int if_idx, struct ether_packet* pkt, size_t pkt_len
+) {
    size_t sent = 0;
+   struct sockaddr_ll socket_address = { 0 };
+#ifdef NET_CON_ECHO
+   int i = 0;
+   bstring buffer = NULL;
+#endif /* NET_CON_ECHO */
 
 #ifdef NET_CON_ECHO
-   printf( "%s\n", bdata( buffer ) );
+   buffer = blk2bstr( pkt, pkt_len );
+   for( i = 0 ; blength( buffer ) > i ; i++ ) {
+      if( '\0' == bchar( buffer, i ) ) {
+         printf( "%02X", (unsigned int)(unsigned char)bchar( buffer, i ) );
+      } else {
+         printf( "%c", bchar( buffer, i ) );
+      }
+   }
+   printf( "\n" );
 #endif /* NET_CON_ECHO */
+
+   socket_address.sll_halen = ETHER_ADDRLEN;
+   socket_address.sll_ifindex = if_idx;
+   memcpy( socket_address.sll_addr, pkt->header.src_mac, ETHER_ADDRLEN );
+
+   if( 0 > sendto( socket, bdata( buffer ), blength( buffer ), 0,
+      (struct sockaddr*)&socket_address, sizeof( struct sockaddr_ll ) )
+   ) {
+      perror( "Unable to send packet" );
+   }
 
    return sent;
 }
--- src/ether.h
+++ src/ether.h
@@ -6,20 +6,59 @@
 #include "bstrlib.h"
 
 #define ETHER_BUFFER_SIZE 1024
+#define ETHER_ADDRLEN 6
+#define ETHER_ADDRLEN_IPV4 4
+
+enum ether_type {
+   ETHER_TYPE_ETHER = 1,
+   ETHER_TYPE_IPV4  = 0x0400,
+   ETHER_TYPE_ARP   = 0x0806,
+   ETHER_TYPE_IPX   = 0x8137,
+   ETHER_TYPE_IPV6  = 0x86dd
+};
+
+enum arp_opcode {
+   ARP_REQUEST    = 0x1,
+   ARP_REPLY      = 0x2,
+   ARP_RARP       = 0x3,
+   ARP_RARP_REPLY = 0x4
+};
 
 struct ether_header {
    uint8_t dest_mac[6];
    uint8_t src_mac[6];
    uint16_t type;
-};
+} __attribute__((packed));
 
 struct ether_packet {
    struct ether_header header;
-   char data[ETHER_BUFFER_SIZE];
-};
+   char data[];
+} __attribute__((packed));
+
+struct arp_header {
+   uint16_t hwtype;
+   uint16_t prototype;
+   uint8_t hwsize;
+   uint8_t protosize;
+   uint16_t opcode;
+} __attribute__((packed));
+
+struct arp_packet_ipv4 {
+   struct arp_header header;
+   uint8_t src_mac[ETHER_ADDRLEN];
+   uint8_t src_ip[ETHER_ADDRLEN_IPV4];
+   uint8_t dest_mac[ETHER_ADDRLEN];
+   uint8_t dest_ip[ETHER_ADDRLEN_IPV4];
+} __attribute__((packed));
 
 struct ether_packet* ether_new_packet(
-   uint8_t src_mac[6], uint8_t dest_mac[6], uint16_t type, bstring data );
+   uint8_t src_mac[6], uint8_t dest_mac[6], enum ether_type type,
+   void* data, size_t data_len );
+struct arp_packet_ipv4* ether_new_arp_packet_ipv4(
+   enum arp_opcode op, const uint8_t* src_mac, const uint8_t* dest_mac,
+   const uint8_t* src_ip, const uint8_t* dest_ip );
+uint16_t ether_ntohs( const uint16_t input );
+uint32_t ether_ntohl( const uint32_t input );
 
 #endif /* ETHER_H */
 
