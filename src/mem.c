c6be786041917e9d3d541d58d2c3ac3ea5f98364
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2019-05-15 13:53:56 -0400

    Starting to see the futility of length checks even for trusted procs, as they defeat any kind of dynamic allocation.

--- src/mem.c
+++ src/mem.c
@@ -6,11 +6,13 @@
 struct mvar {
    int pid;
    int mid;
-   int size;
+   int len;  /* Used. */
+   int size; /* Allocated. */
 };
 
 static uint8_t g_mheap[MEM_HEAP_SIZE];
 static int g_mheap_top = 0;
+static uint8_t g_mbuffer[MEM_BUFFER_SIZE];
 
 void minit() {
    mzero( g_mheap, MEM_HEAP_SIZE );
@@ -58,43 +60,26 @@ struct mstring* mfromcstr( char* str ) {
 }
 */
 
-void mset( int pid, int mid, void* ptr, int len ) {
-   struct mvar* var = NULL;
-
-   if( g_mheap_top + sizeof( struct mvar ) +  len > MEM_HEAP_SIZE ) {
-      /* Not enough heap available! */
-      return;
-   }
-
-   /* Create the variable. */
-   var = (struct mvar*)&(g_mheap[g_mheap_top]);
-   var->pid = pid;
-   var->mid = mid;
-   var->size = len;
-   mcopy(
-      &(g_mheap[g_mheap_top + sizeof( struct mvar )]), /* Past the header. */
-      ptr, len );
-
-   /* Advance the heap top. */
-   g_mheap_top += len;
+/* "Secure" working buffer shared by tasks. */
+uint8_t* mbuffer() {
+   mzero( g_mbuffer, MEM_BUFFER_SIZE );
+   return g_mbuffer;
 }
 
-/* Copy the result into a buffer provided by the process, for security 
- * checks.
- */
-void mget( int pid, int mid, void* buffer, int buffer_sz ) {
+/* Get the heap position for a variable. Used in public functions below. */
+static int mget_pos( int pid, int mid ) {
    struct mvar* var_iter = (struct mvar*)g_mheap;
    int mheap_addr_iter = 0;
 
    if( 0 >= g_mheap_top ) {
       /* Heap is empty. */
-      return;
+      return -1;
    }
 
    while( pid != var_iter->pid && mid != var_iter->mid ) {
       if( mheap_addr_iter < g_mheap_top ) {
          /* Unable to find it and ran out of heap! */
-         return;
+         return -1;
       }
 
       /* Advance past the variable and metadara. */
@@ -107,16 +92,69 @@ void mget( int pid, int mid, void* buffer, int buffer_sz ) {
 
    if( pid != var_iter->pid || mid != var_iter->mid ) {
       /* Unable to find it! */
-      return;
+      return -1;
    }
 
-   if( buffer_sz < var_iter->size ) {
+   return mheap_addr_iter;
+}
+
+void mset( int pid, int mid, void* ptr, int len ) {
+   int mheap_addr_iter = 0;
+   struct mvar* var = NULL;
+   int size_diff = 0;
+
+   mheap_addr_iter = mget_pos( pid, mid );
+   if( 0 > mheap_addr_iter ) {
+      /* Not found. Create it. */
+      if( g_mheap_top + sizeof( struct mvar ) +  len > MEM_HEAP_SIZE ) {
+         /* Not enough heap available! */
+         return;
+      }
+
+      /* Create the variable. */
+      var = (struct mvar*)&(g_mheap[g_mheap_top]);
+      var->pid = pid;
+      var->mid = mid;
+      var->size = len;
+      mcopy(
+         &(g_mheap[g_mheap_top + sizeof( struct mvar )]), /* Past the header. */
+         ptr, len );
+
+      /* Advance the heap top. */
+      g_mheap_top += len;
+   } else {
+      /* Found, so update it. */
+      var = (struct mvar*)&(g_mheap[mheap_addr_iter]);
+
+      size_diff = len - var->size;
+      if( 0 < size_diff && g_mheap_top + size_diff > MEM_HEAP_SIZE ) {
+         /* Not enough heap space! */
+         return;
+      }
+   }
+}
+
+/* Copy the result into a buffer provided by the process, for security 
+ * checks.
+ */
+void mget( int pid, int mid, void* buffer, int buffer_sz ) {
+   int mheap_addr_iter = 0;
+   struct mvar* var = NULL;
+
+   mheap_addr_iter = mget_pos( pid, mid );
+   if( 0 > mheap_addr_iter ) {
+      /* Not found! */
+      return;
+   }
+   var = (struct mvar*)&(g_mheap[mheap_addr_iter]);
+
+   if( buffer_sz < var->size ) {
       /* Buffer too small! */
       return;
    }
 
    /* Return the value, and ONLY the value. */
    mheap_addr_iter += sizeof( struct mvar );
-   mcopy( buffer, &(g_mheap[mheap_addr_iter]), var_iter->size );
+   mcopy( buffer, &(g_mheap[mheap_addr_iter]), var->size );
 }
 
