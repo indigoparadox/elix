8086d5891ad0ef3af91e18f656a252f0a942b5c4
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-05-15 03:13:47 -0400

    Added super-rough MSP430 stuff.

---
+++ src/msp430/display.c
@@ -0,0 +1,21 @@
+
+#include "../display.h"
+
+#include <stdio.h>
+
+void display_set_colors( uint8_t fg, uint8_t bg ) {
+}
+
+void display_init() {
+
+}
+
+void display_puts( const char* str, int len ) {
+}
+
+void display_putc_at( char c, int x, int y ) {
+}
+
+void display_putc( char c ) {
+}
+
