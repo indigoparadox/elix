67d6272c824156e18b5638f6d7a091d3e953bfb3
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-06-03 23:24:57 -0400

    Placeholder x86 symbols to compile.

--- src/x86/keyboard.c
+++ src/x86/keyboard.c
@@ -1,2 +1,18 @@
 
+#include "../keyboard.h"
+#include "../kernel.h"
+
+void keyboard_init() {
+}
+
+void keyboard_shutdown() {
+}
+
+int keyboard_hit() {
+}
+
+unsigned char keyboard_getc() {
+   return 0;
+}
+
 
