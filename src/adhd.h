d419336af166d2bb65e7bc7853a91dc9b58bec2a
indigoparadox <indigo.repo@interfinitdynamics.info>
2019-06-04 12:59:34 -0400

    Net task kills self on socket failure.

--- src/adhd.h
+++ src/adhd.h
@@ -8,14 +8,18 @@
 
 #define ADHD_TASKS_MAX 5
 
-#define TASK_PID uint8_t
-#define TASK_RETVAL uint8_t
+typedef uint8_t TASK_PID;
+typedef uint8_t TASK_RETVAL;
+
+#define RETVAL_OK 0
+#define RETVAL_KILL 255
 
 struct adhd_task;
 
 TASK_PID adhd_add_task( TASK_RETVAL (*callback)( TASK_PID ) );
 TASK_PID adhd_get_tasks_len();
 TASK_RETVAL adhd_call_task( TASK_PID pid );
+void adhd_kill_task( TASK_PID pid );
 void adhd_wait( BITFIELD status, BITFIELD condition );
 
 #endif /* ADHD_H */
