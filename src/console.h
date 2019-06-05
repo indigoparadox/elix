67441a8a16c5f804c27b6b47869fdfd718ac9efe
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2019-06-05 12:48:06 -0400

    Starting to rough out command handling.

--- src/console.h
+++ src/console.h
@@ -12,7 +12,6 @@
 #include "alpha.h"
 
 #define REPL_LINE_SIZE_MAX 20
-#define REPL_COMMANDS_MAX 5
 
 #define CONSOLE_OP_NOOP 0x0
 #define CONSOLE_OP_PUSH 0x1
@@ -25,21 +24,9 @@
 
 #define console_const( id, str ) astring_const( id, str )
 
-struct repl_command {
-   struct astring* command;
-   void (*callback)();
-};
-
-void tregcmd( struct repl_command* );
 void tprintf( const struct astring* pattern, ... );
 void tputs( const struct astring* str );
 TASK_RETVAL trepl_task( TASK_PID pid );
-
-#ifdef CONSOLE_C
-struct repl_command g_repl_commands[REPL_COMMANDS_MAX];
-#else
-extern struct repl_command g_repl_commands[REPL_COMMANDS_MAX];
-#endif /* CONSOLE_C */
 
 #else /* USE_CONSOLE */
 
