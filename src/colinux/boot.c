8c34091d75c17797c7b953ced7dd26f0c0aca652
indigoparadox <indigo.repo@interfinitdynamics.info>
2019-07-03 15:21:22 -0400

    Smooth over the edges to work disk support in.

--- src/colinux/boot.c
+++ src/colinux/boot.c
@@ -1,8 +1,33 @@
 
 #include "../kernel.h"
 
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
 int main( void ) {
+   int img_handle = 0;
+   uint8_t* img_map = NULL;
+   struct stat img_stat;
+   size_t img_size = 0;
+
+   stat( "testimg.img", &img_stat );
+   img_size = img_stat.st_size;
+
+   img_handle = open( "testimg.img", O_RDONLY, 0 );
+   assert( 0 < img_handle );
+
+   img_map = mmap( NULL, img_size, PROT_READ,
+      MAP_PRIVATE | MAP_POPULATE, img_handle, 0 );
+   assert( MAP_FAILED != img_map );
+   close( img_handle );
+   disk_set_ptr( 0, img_map );
+
    kmain();
+
+   munmap( img_map, img_size );
+
    return 0;
 }
 
