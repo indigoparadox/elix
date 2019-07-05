fb1acc8589d999cf77c635bc86f6ec4e4d215e24
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2019-07-05 11:40:50 -0400

    Only gather args if we use them.

--- src/kernel.h
+++ src/kernel.h
@@ -11,7 +11,11 @@
 
 #define KERNEL_MID_CLI 5
 
+#ifdef USE_EXT_CLI
 int kmain( int argc, char** argv );
+#else
+int kmain();
+#endif /* USE_EXT_CLI */
 
 #ifdef KERNEL_C
 uint8_t g_system_state = SYSTEM_RUNNING;
