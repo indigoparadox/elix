1e3f577ca06047377759b659b49777ab21def34b
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2019-07-09 16:05:59 -0400

    More hacking to boot on X86.

---
+++ src/x86/uart.c
@@ -0,0 +1,27 @@
+
+#include "../uart.h"
+
+uint8_t uart_init( uint8_t index ) {
+   return 0;
+}
+
+void uart_clear( uint8_t index ) {
+}
+
+unsigned char uart_getc( uint8_t dev_index ) {
+   return '\0';
+}
+
+uint8_t uart_hit( uint8_t dev_index ) {
+   return 0;
+}
+
+void uart_putc( uint8_t dev_index, const char c ) {
+}
+
+void uart_soft_pause( uint8_t index ) {
+}
+
+void uart_soft_resume( uint8_t index ) {
+}
+
