4f19c2678729bb578ceb77f26b39dce2b1901d92
indigoparadox <indigo.repo@interfinitdynamics.info>
2019-07-10 10:25:53 -0400

    Updated msp430 display.

--- src/msp430/display.c
+++ src/msp430/display.c
@@ -1,10 +1,13 @@
 
 #include "../display.h"
+#include "../kernel.h"
+#include "../io.h"
 
 void display_set_colors( uint8_t fg, uint8_t bg ) {
 }
 
-void display_init() {
+__attribute__( (constructor( CTOR_PRIO_DISPLAY )) )
+static void display_init() {
 
 }
 
@@ -14,7 +17,8 @@ void display_putc_at( char c, int x, int y ) {
 void display_putc( uint8_t dev_index, char c ) {
 }
 
-void display_shutdown() {
+__attribute__( (destructor( CTOR_PRIO_DISPLAY )) )
+static void display_shutdown() {
 
 }
 
