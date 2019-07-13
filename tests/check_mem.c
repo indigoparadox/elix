cc9710af47c85e2e27c4f2782678eb8c2abe67f9
indigoparadox <indigo.repo@interfinitdynamics.info>
2019-07-13 11:15:54 -0400

    Implemented and tested mfree.

--- tests/check_mem.c
+++ tests/check_mem.c
@@ -2,6 +2,7 @@
 #include <check.h>
 #include <stdint.h>
 #include "../src/mem.h"
+#include "../src/console.h"
 
 #include <stdlib.h>
 
@@ -17,6 +18,10 @@
 #define CHECK_MEM_SHIFT_OFFSET 5
 
 #define CHECK_STR_COUNT 4
+
+#define CHECK_MID_DELETEME 99
+#define CHECK_SZ_DELETEME 20
+char g_chk_deleteme[CHECK_SZ_DELETEME] = "Delete this string.";
 
 char* g_chk_str[4] = {
    "This is a test string.",
@@ -47,31 +52,28 @@ static void setup_mem() {
    int offset = 0;
    int i = 0;
 
-#ifdef CHECK_VERBOSE
-   printf( "Setting up...\n" );
-#endif /* CHECK_VERBOSE */
-   g_mheap_top = 0;
+   minit();
+
+   mem_ptr = mget( CHECK_PID, CHECK_MID_DELETEME, CHECK_SZ_DELETEME );
+   mcopy( mem_ptr, g_chk_deleteme, CHECK_SZ_DELETEME );
+   mfree( CHECK_PID, CHECK_MID_DELETEME );
 
    /* Place the strings on the heap to check. */
    for( i = 0 ; 3 > i ; i++ ) {
-#ifdef CHECK_VERBOSE
-      printf( "Adding \"%s\" to main heap...\n", g_chk_str[i] );
-#endif /* CHECK_VERBOSE */
-      mset( CHECK_PID, i + 1, g_chk_len[i], g_chk_str[i] );
+      mem_ptr = mget( CHECK_PID, i + 1, g_chk_len[i] );
+      ck_assert_ptr_ne( mem_ptr, NULL );
+      mcopy( mem_ptr, g_chk_str[i], g_chk_len[i] );
+      ck_assert_str_eq( mem_ptr, g_chk_str[i] );
    }
 
    /* Fill up a simulated heap to compare. */
    g_theap = calloc( 1, MEM_HEAP_SIZE );
    i = 0;
    do {
-#ifdef CHECK_VERBOSE
-      printf( "Adding \"%s\" to sim heap...\n", g_chk_str[i] );
-#endif /* CHECK_VERBOSE */
       var_header = (struct mvar*)&(g_theap[offset]);
-      ck_assert_ptr_ne( var_header, NULL );
       var_header->pid = CHECK_PID;
       var_header->mid = i + 1;
-      var_header->size = g_chk_len[i];
+      var_header->sz = g_chk_len[i];
       memcpy( &(var_header->data[0]), g_chk_str[i], g_chk_len[i] );
       offset += sizeof( struct mvar ) + g_chk_len[i];
       i++;
@@ -79,9 +81,6 @@ static void setup_mem() {
 }
 
 static void teardown_mem() {
-#ifdef CHECK_VERBOSE
-   printf( "Tearing down...\n" );
-#endif /* CHECK_VERBOSE */
    free( g_theap );
 }
 
@@ -104,7 +103,7 @@ START_TEST( test_vars ) {
    ck_assert_ptr_ne( var_header, NULL );
    ck_assert_int_eq( var_header->mid, _i );
    ck_assert_int_eq( var_header->pid, CHECK_PID );
-   //ck_assert_int_eq( var_header->size, g_chk_len[_i] );
+   //ck_assert_int_eq( var_header->sz, g_chk_len[_i] );
    //ck_assert_str_eq( var_header->data, g_chk_str[_i] );
 }
 END_TEST
@@ -120,7 +119,7 @@ START_TEST( test_mshift ) {
    var_header = (struct mvar*)&(g_mheap[CHECK_MEM_SHIFT_OFFSET]);  
    ck_assert_int_eq( var_header->pid, CHECK_PID );
    ck_assert_int_eq( var_header->mid, CHECK_MID_TEST1 );
-   ck_assert_int_eq( var_header->size, g_chk_len[0] );
+   ck_assert_int_eq( var_header->sz, g_chk_len[0] );
    
 }
 END_TEST
@@ -131,35 +130,29 @@ END_TEST
 START_TEST( test_mget_overwrite ) {
    uint8_t* mem_ptr = NULL;
 
-   mem_ptr = (uint8_t*)mget( CHECK_PID, _i + 1, g_chk_len[_i] + 5 );
+   mem_ptr = mget( CHECK_PID, _i + 1, g_chk_len[_i] + 5 );
    ck_assert_ptr_ne( mem_ptr, NULL );
-   ck_assert_str_eq( (char*)mem_ptr, g_chk_str[_i] );
+   ck_assert_str_eq( mem_ptr, g_chk_str[_i] );
 }
 END_TEST
 
 START_TEST( test_mget_pos ) {
-   MEMLEN_T should_pos = 0;
-   MEMLEN_T actual_pos = 0;
+   MEMLEN_T pos = 0;
 
-   char mem_fn_buffer[256];
-
-   snprintf( mem_fn_buffer, 255, "mchk_pos_%d.bin", _i );
-   msave( mem_fn_buffer );
-
-   actual_pos = mget_pos( CHECK_PID, _i );
+   pos = mget_pos( CHECK_PID, _i );
    switch( _i ) {
       case 1:
-         ck_assert_int_eq( actual_pos, 0 );
+         ck_assert_int_eq( pos, 0 );
          break;
 
       case 2:
-         should_pos = sizeof( struct mvar ) + g_chk_len[0];
-         ck_assert_int_eq( actual_pos, should_pos );
+         ck_assert_int_eq( pos,
+            sizeof( struct mvar ) + g_chk_len[0]  );
          break;
 
       case 3:
-         should_pos = (2 * sizeof( struct mvar )) + g_chk_len[0] + g_chk_len[1];
-         ck_assert_int_eq( actual_pos, should_pos );
+         ck_assert_int_eq( pos,
+            (2 * sizeof( struct mvar )) + g_chk_len[0] + g_chk_len[1] );
          break;
    }
 }
@@ -205,7 +198,7 @@ START_TEST( test_mget_pid_match ) {
    var_header = (struct mvar*)&(g_mheap[offset]);
    ck_assert_int_eq( var_header->pid, CHECK_PID + 1 );
    ck_assert_int_eq( var_header->mid, CHECK_MID_TEST1 );
-   ck_assert_int_eq( var_header->size, CHECK_STR_SZ );
+   ck_assert_int_eq( var_header->sz, CHECK_STR_SZ );
    offset += sizeof( struct mvar );
    for( i = 0 ; CHECK_STR_SZ > i ; i++ ) {
       test_heap = g_mheap[offset + i];
@@ -219,7 +212,7 @@ START_TEST( test_mget_pid_match ) {
    var_header = mget( CHECK_PID + 1, CHECK_MID_TEST1, CHECK_STR_SZ );
    ck_assert_int_eq( var_header->pid, CHECK_PID + 1 );
    ck_assert_int_eq( var_header->mid, CHECK_MID_TEST1 );
-   ck_assert_int_eq( var_header->size, CHECK_STR_SZ );
+   ck_assert_int_eq( var_header->sz, CHECK_STR_SZ );
    for( i = 0 ; CHECK_STR_SZ > i ; i++ ) {
       test_heap = var_header->data[i];
       test_sample = g_chk_str_1[i];
@@ -232,7 +225,7 @@ START_TEST( test_mget_pid_match ) {
    var_header = (struct mvar*)&(g_mheap[offset]);
    ck_assert_int_eq( var_header->pid, CHECK_PID );
    ck_assert_int_eq( var_header->mid, CHECK_MID_TEST2 );
-   ck_assert_int_eq( var_header->size, CHECK_STR_SZ );
+   ck_assert_int_eq( var_header->sz, CHECK_STR_SZ );
    offset += sizeof( struct mvar );
    for( i = 0 ; CHECK_STR_SZ > i ; i++ ) {
       test_heap = g_mheap[offset + i];
@@ -254,7 +247,7 @@ START_TEST( test_mget_pid_match ) {
    var_header = (struct mvar*)&(g_mheap[offset]);
    ck_assert_int_eq( var_header->pid, CHECK_PID );
    ck_assert_int_eq( var_header->mid, CHECK_MID_TEST1 );
-   ck_assert_int_eq( var_header->size, CHECK_STR_SZ_2 );
+   ck_assert_int_eq( var_header->sz, CHECK_STR_SZ_2 );
    offset += sizeof( struct mvar );
    for( i = 0 ; CHECK_STR_SZ_2 > i ; i++ ) {
       test_heap = g_mheap[i + offset];
@@ -319,41 +312,6 @@ START_TEST( test_mset_line ) {
 END_TEST
 #endif
 
-/* Tests: Editing */
-
-static void setup_medit() {
-   mset( CHECK_PID, 8, g_chk_len[0], &(g_chk_str[0]) );
-}
-
-static void teardown_medit() {
-
-}
-
-START_TEST( test_meditprop ) {
-   const char* test_str = NULL;
-   char test_char = 'q' + _i;
-
-   test_str = mget( CHECK_PID, 8, g_chk_len[0] );
-   ck_assert_ptr_ne( test_str, NULL );
-   meditprop( CHECK_PID, 8, _i, sizeof( char ), &test_char );
-   ck_assert_int_eq( test_str[_i], 'q' + _i );
-}
-END_TEST
-
-START_TEST( test_mgetprop ) {
-   char* test_str = NULL;
-   char get_prop_c = '\0';
-
-   test_str = (char*)mget( CHECK_PID, 8, MGET_NO_CREATE );
-   ck_assert_ptr_ne( test_str, NULL );
-
-   mgetprop( CHECK_PID, 8, _i, sizeof( char ), &get_prop_c );
-
-   ck_assert_int_eq( test_str[_i], get_prop_c );
-   ck_assert_int_ne( get_prop_c, '\0' );
-}
-END_TEST
-
 Suite* mem_suite( void ) {
    Suite* s;
    TCase* tc_layout;
@@ -361,7 +319,6 @@ Suite* mem_suite( void ) {
    //TCase* tc_set;
    TCase* tc_overwrite;
    //TCase* tc_pid;
-   TCase* tc_edit;
 
    s = suite_create( "mem" );
 
@@ -371,14 +328,13 @@ Suite* mem_suite( void ) {
    tc_shift = tcase_create( "Shift" );
    tc_overwrite = tcase_create( "Overwrite" );
    //tc_pid = tcase_create( "PID" );
-   tc_edit = tcase_create( "Edit" );
 
    /* Tests: Set */
    
 
    /* Tests: Layout */
    tcase_add_checked_fixture( tc_layout, setup_mem, teardown_mem );
-   tcase_add_loop_test( tc_layout, test_bytes, 0, 320 );
+   tcase_add_loop_test( tc_layout, test_bytes, 0, MEM_HEAP_SIZE - 1 );
    tcase_add_loop_test( tc_layout, test_vars, 1, CHECK_STR_COUNT );
 
    /* Tests: Shift */
@@ -398,15 +354,9 @@ Suite* mem_suite( void ) {
    //tcase_add_test( tc_core, test_mget_pid_match );
    //tcase_add_test( tc_core, test_mset_line );
 
-   /* Tests: Edit */
-   tcase_add_checked_fixture( tc_overwrite, setup_medit, teardown_medit );
-   tcase_add_loop_test( tc_edit, test_meditprop, 1, 4 );
-   tcase_add_loop_test( tc_edit, test_mgetprop, 1, 4 );
-
    suite_add_tcase( s, tc_overwrite );
    suite_add_tcase( s, tc_layout );
    suite_add_tcase( s, tc_shift );
-   suite_add_tcase( s, tc_edit );
 
    return s;
 }
