ab1df4f5dcac11f3fa7ffa545321b35c6fbce584
indigoparadox <indigo.repo@interfinitydynamics.info>
2021-06-05 10:15:23 -0400

    Assembler can loop.

--- src/assm.c
+++ src/assm.c
@@ -13,7 +13,16 @@
 #define STATE_PARAMS    2
 #define STATE_CHAR      3
 
+struct label {
+   size_t ipc;
+   char name[BUF_SZ + 1];
+   struct label* next;
+};
+
+struct label* g_labels = NULL;
+size_t g_ipc = 0;
 int g_section = 0;
+int g_instr = 0;
 int g_state = STATE_NONE;
 char g_token[BUF_SZ + 1] = { 0 };
 size_t g_token_len = 0;
@@ -25,8 +34,52 @@ void reset_token() {
    g_token_len = 0;
 }
 
+void add_label( char* name, int ipc ) {
+   struct label* label_iter = g_labels;
+
+   if( NULL == g_labels ) {
+      g_labels = calloc( 1, sizeof( struct label ) );
+      assert( NULL != g_labels );
+      label_iter = g_labels;
+   } else {
+      while( NULL != label_iter->next ) {
+         label_iter = label_iter->next;
+      }
+      label_iter->next = calloc( 1, sizeof( struct label ) );
+      assert( NULL != label_iter->next );
+      label_iter = label_iter->next;
+   }
+
+   strncpy( label_iter->name, name, BUF_SZ );
+   label_iter->ipc = ipc;
+
+   printf( "label: %s @ %d\n", name, ipc );
+}
+
+size_t get_label_ipc( char* label ) {
+   struct label* label_iter = g_labels;
+
+   while( 0 != strncmp( label_iter->name, label, strlen( label_iter->name ) ) ) {
+      printf( "searching for %s, found %s...\n", label, label_iter->name );
+      label_iter = label_iter->next;
+   }
+
+   assert( NULL != label_iter );
+
+   printf( "label for %s: %c\n", label, (unsigned char)(label_iter->ipc) );
+
+   return label_iter->ipc;
+}
+
+void write_bin_instr_or_data( char c ) {
+   fwrite( &c, sizeof( char ), 1, g_bin_file );
+   g_ipc++;
+   printf( "ipc: %ld\n", g_ipc );
+}
+
 void process_char( char c ) {
    char buf_out[BUF_SZ + 1] = { 0 };
+   size_t label_ipc = 0;
 
    switch( c ) {
    case '.':
@@ -41,28 +94,29 @@ void process_char( char c ) {
       if( STATE_SECTION == g_state ) {
          printf( "section: %s\n", g_token );
          if( 0 == strncmp( "cpu", g_token, 3 ) ) {
-            buf_out[0] = VM_INSTR_SECT;
-            buf_out[1] = VM_SECTION_CPU;
-            fwrite( &buf_out, sizeof( char ), 2, g_bin_file );
+            write_bin_instr_or_data( VM_INSTR_SECT );
+            write_bin_instr_or_data( VM_SECTION_CPU );
             g_section = VM_SECTION_CPU;
          } else if( 0 == strncmp( "data", g_token, 4 ) ) {
-            buf_out[0] = VM_INSTR_SECT;
-            buf_out[1] = VM_SECTION_DATA;
-            fwrite( &buf_out, sizeof( char ), 2, g_bin_file );
+            write_bin_instr_or_data( VM_INSTR_SECT );
+            write_bin_instr_or_data( VM_SECTION_DATA );
             g_section = VM_SECTION_DATA;
          }
          reset_token();
          g_state = STATE_NONE;
+      } else if( STATE_NONE == g_state && VM_SECTION_CPU == g_section ) {
+         add_label( g_token, g_ipc );
+         reset_token();
       }
       break;
 
    case '\'':
       if( VM_SECTION_CPU == g_section ) {
          if( STATE_PARAMS == g_state ) {
-            printf( "params state\n" );
+            printf( "state params\n" );
             g_state = STATE_CHAR;
          } else if( STATE_CHAR == g_state ) {
-            printf( "no state\n" );
+            printf( "state none\n" );
             reset_token();
             g_state = STATE_NONE;
          }
@@ -76,24 +130,40 @@ void process_char( char c ) {
          //if( STATE_PARAMS == g_state ) {
          //   g_state = STATE_NONE;
          if( 0 == strncmp( "push", g_token, 4 ) ) {
-            buf_out[0] = VM_INSTR_PUSH;
-            fwrite( &buf_out, sizeof( char ), 1, g_bin_file );
+            write_bin_instr_or_data( VM_INSTR_PUSH );
             g_state = STATE_PARAMS;
+            g_instr = VM_INSTR_PUSH;
          } else if( 0 == strncmp( "syscall", g_token, 7 ) ) {
-            buf_out[0] = VM_INSTR_SYSC;
-            fwrite( &buf_out, sizeof( char ), 1, g_bin_file );
+            write_bin_instr_or_data( VM_INSTR_SYSC );
             g_state = STATE_PARAMS;
+            g_instr = VM_INSTR_SYSC;
+         } else if( 0 == strncmp( "goto", g_token, 4 ) ) {
+            write_bin_instr_or_data( VM_INSTR_GOTO );
+            g_state = STATE_PARAMS;
+            g_instr = VM_INSTR_GOTO;
          }
-         printf( "instr: %s\n", g_token );
+         if( 0 < g_token_len ) {
+            printf( "instr: %s\n", g_token );
+         }
          reset_token();
          break;
+      } else if(
+         VM_SECTION_CPU == g_section &&
+         VM_INSTR_GOTO == g_instr &&
+         STATE_PARAMS == g_state &&
+         0 < g_token_len
+      ) {
+         label_ipc = get_label_ipc( g_token );
+         write_bin_instr_or_data( (unsigned char)label_ipc );
+         g_state = STATE_NONE;
+         g_instr = 0;
       } else if( VM_SECTION_CPU == g_section && STATE_PARAMS == g_state ) {
          if( 0 < g_token_len ) {
             printf( "param: %s\n", g_token );
             if( 0 == strncmp( g_token, "putc", 4 ) ) {
-               buf_out[0] = VM_SYSC_PUTC;
-               fwrite( &buf_out, sizeof( char ), 1, g_bin_file );
+               write_bin_instr_or_data( VM_SYSC_PUTC );
                g_state = STATE_NONE;
+               g_instr = 0;
             }
             reset_token();
          }
@@ -102,7 +172,8 @@ void process_char( char c ) {
 
    default:
       if( STATE_CHAR == g_state ) {
-         fwrite( &c, sizeof( char ), 1, g_bin_file );
+         printf( "c: %c\n", c );
+         write_bin_instr_or_data( c );
       } else {
          assert( g_token_len < BUF_SZ );
          g_token[g_token_len] = c;
@@ -111,7 +182,7 @@ void process_char( char c ) {
       break;
    }
    
-   printf( "c: %c\n", c );
+   //printf( "c: %c\n", c );
 }
 
 int main( int argc, char* argv[] ) {
